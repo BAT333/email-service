b6887375266d49cf8b4708995d70a30ce20ca3f4
RafaelBat-alves <152424427+RafaelBat-alves@users.noreply.github.com>
2024-09-02 18:41:10 -0300

    SpringDoc Api Documentation

--- src/main/java/com/example/email/config/SpringDocConfig.java
+++ src/main/java/com/example/email/config/SpringDocConfig.java
@@ -3,6 +3,7 @@ package com.example.email.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,11 @@ public class SpringDocConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .info(
+                        new Info().title("Email service api")
+                                .description("THIS PART OF THE APS IS RESPONSIBLE FOR SENDING MESSAGES VIA EMAIL")
+                                .version("v0.0.1")
+                )
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
                                 new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
